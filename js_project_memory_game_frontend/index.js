8c6c90d5f5f99a4eb2439d6829b3154f19177e6e
kierra <51520037+kierrakay@users.noreply.github.com>
2020-07-10 17:10:42 -0400

    created audio controller class

---
+++ js_project_memory_game_frontend/index.js
@@ -0,0 +1,58 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('Assets/Audio/Summer.mp3');
+        this.flipSound = new Audio('Assets/Audio/flip.wav');
+        this.matchSound = new Audio('Assets/Audio/match.wav');
+        this.victorySound = new Audio('Assets/Audio/victory.wav');
+        this.gameOverSound = new Audio('Assets/Audio/gameover.wav');
+        this.bgMusic.voulme = 0.5;
+        this.bgMusic.loop = true;
+    }
+
+    startMusic() {
+        this.bgMusic.play();
+    }
+
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime - 0;
+    }
+
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.stopMusic()
+        this.victorySound.play();
+    }
+    gameover() {
+        this.stopMusic()
+        this.gameOverSound.play();
+    }
+}
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            //game.startGame();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', ()=> {
+            //game.flipCard(card);
+        });
+    });
+}
+
+if(document.readyState ==='loading') {
+    document.addEventListener('DOMContentLoaded',ready());
+} else {
+ready();
+}
